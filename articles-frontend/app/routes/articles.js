5d41b936c535c40d6af394adf2b00be01e47e078
monsteronfire <monsteronfire@gmail.com>
2016-09-22 23:41:03 +0800

    Update active-model-serializer gem * Also configure articles route on frontend

--- articles-frontend/app/routes/articles.js
+++ articles-frontend/app/routes/articles.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.query('article', { page:{
+        number: params.page,
+        size: params.size
+      }
+    });
+  },
+
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    size: {
+      refreshModel: true
+    }
+  }
 });
